a16039e3b2a643b35846f035a6c72c447837a6a1
Mina Trevizo <trevizomina@gmail.com>
2020-06-12 23:31:17 +0000

    Basic information for starting Turtle library

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-from turtle import* 
-turtle = Turtle()
-turtle.forward(50)
-turtle.left(90)
-turtle.forward(50)
-turtle.left(90)
-turtle.forward(50)
-turtle.left(90)
-turtle.forward(50)
-turtle.left(90)
 
-#added comment to test github changes
+""" 
+    visit the website below to get more information about the functions we use!
+
+            https://docs.python.org/2/library/turtle.html
+    
+    We will be using the functions from this library a lot for these graphics!
+
+"""
+from turtle import* 
