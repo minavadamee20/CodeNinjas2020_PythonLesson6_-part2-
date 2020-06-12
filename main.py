94ed636f6a645874cebc164ef9d450c81950bbb5
Mina Trevizo <trevizomina@gmail.com>
2020-06-12 23:24:54 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from turtle import* 
+turtle = Turtle()
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
